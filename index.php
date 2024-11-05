a9394a3947ff740c71ee7887879a357a488ee2db
K D P Hasendra <k.d.p.hasendra@gmail.com>
2024-11-05 14:39:24 +0530

    Intial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+
+    </head>
+    <body>
+      
+    </body>
+
+
+
+</html>
---
+++ view/login.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row" style="height:100px">
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2" style="background-color:green;height:300px">
+                    &nbsp;
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
