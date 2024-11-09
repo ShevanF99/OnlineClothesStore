ade1afbd7f920e7a95db639ef6bc746792625531
K D P Hasendra <k.d.p.hasendra@gmail.com>
2024-11-09 11:33:50 +0530

    09-11-2024 Class

--- view/login.php
+++ view/login.php
@@ -6,11 +6,54 @@
         <div class="container">
             <div class="row" style="height:100px">
             </div>
+        <form action="" method="post">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2" style="background-color:green;height:300px">
-                    &nbsp;
+                <div class="col-md-8 col-md-offset-2 panel panel-default" style="height:300px">
+                    <div class="col-md-6" style="height:300px">
+                    <img src="../images/logo.png" height="300px"/>
+                    </div>
+                    <div class="col-md-6" style="height:300px">
+                        <div class="row">
+                            &nbsp;
+                        </div>
+                        <div class="row">
+                        <label style="font-size:18px">Sign Into Your Account</label>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <span class="input-group">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-user"></span>
+                                    </span>
+                                    <input type="email" id="loginusername" name="loginusername" class="form-control" required="required"/>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            &nbsp;
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <span class="input-group">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-lock"></span>
+                                    </span>
+                                    <input type="password" id="loginpassword" name="loginpassword" class="form-control" required="required"/>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            &nbsp;
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <input type="submit" name="submit" class="btn btn-primary btn-block" style="background-color:#170680"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </form>
         </div>
     </body>
 
