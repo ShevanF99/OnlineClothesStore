4754112c5c5ca2913e8b15dffa1a6b2c3a59416e
K D Pooraka Hasendra <k.d.p.hasendra@gmail.com>
2024-11-30 11:32:46 +0530

    30-11-2024 Class

--- model/login_model.php
+++ model/login_model.php
@@ -10,7 +10,8 @@ public function validateUser($login_username,$login_password){
 
     $con=$GLOBALS["con"];
     $login_password=sha1($login_password);
-    $sql = "SELECT u.user_id,user_role FROM user u, login l WHERE u.user_id=l.user_id 
+    $sql = "SELECT u.user_id, u.user_role, r.role_name, u.user_fname, u.user_lname FROM user u, login l, role r 
+            WHERE u.user_id=l.user_id AND  r.role_id =u.user_role 
             AND l.login_username='$login_username' AND l.login_password='$login_password'";
 
     $result = $con->query($sql) or die($con->error);
--- view/dashboard.php
+++ view/dashboard.php
@@ -1,22 +1,50 @@
+<?php
+
+include_once '../commons/session.php';
+
+//get user information from session
+$userrow=$_SESSION["user"];
+
+?>
+
+
+
+
 <html>
     <head>
     <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css"/>
     </head>
     <body>
         <div class="container">
-            <div class="row" style="height:100px">
-                <div class="col-md-3">
-                    <img src="../images/logo.png" width="80px" heigth="100px"/>
-                </div>
-                <div class="col-md-6">
-                    Director
-                </div>
-                <div class="col-md-3">
-                    <a href="" class="btn btn-primary">Logout</a>
-                </div>
+            <div class="row" style="height:50px">
 
             </div>
-            
+            <div class="row" style="height:60px">
+                <div class="col-md-3">
+                    <img src="../images/logo.png" width="80px" heigth="60px"/>
+                </div>
+                <div class="col-md-6">
+                    <h1 align="center">Online Colthestore</h1>
+                </div>
+                <div class="col-md-1 col-md-offset-2">
+                </div>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-md-3">
+                    <?php
+                        echo ucwords($userrow["user_fname"]." ".$userrow["user_lname"]);
+                    ?>
+                </div>
+                <div class="col-md-6">
+                    <h4 align="center">Online Colthestore</h4>
+                </div>
+                <div class="col-md-1 col-md-offset-2">
+                    </br>
+                    <a href="" class="btn btn-primary">Logout</a>
+                </div>
+            </div>
+            </br>     
         </div>
     </body>
     <script src="../js/jquery-3.7.1.js"></script>
