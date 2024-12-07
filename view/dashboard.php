c154c4cc3493c925cac2f6e5548159984334cfd3
K D Pooraka Hasendra <k.d.p.hasendra@gmail.com>
2024-12-07 11:37:12 +0530

    07-12-2024 Class

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,14 +1,17 @@
 <?php
 
 include_once '../commons/session.php';
+include_once '../model/module_model.php';
 
 //get user information from session
 $userrow=$_SESSION["user"];
 
+$moduleObj = new Module();
+
+$moduleResult = $moduleObj->getAllModules();
+
+
 ?>
-
-
-
 
 <html>
     <head>
@@ -40,11 +43,29 @@ $userrow=$_SESSION["user"];
                     <h4 align="center">Online Colthestore</h4>
                 </div>
                 <div class="col-md-1 col-md-offset-2">
-                    </br>
                     <a href="" class="btn btn-primary">Logout</a>
                 </div>
             </div>
-            </br>     
+            <hr/>
+            <?php
+                while ($module_row=$moduleResult->fetch_assoc())
+                {
+            ?> 
+                <div class="col-md-4">
+                    <a href="<?php echo $module_row["module_url"]?>" style="text-decoration:none;color:#fff">
+                        <div class="panel" style="height: 170px;background-color:#45a88f">
+                            <h1 align="center">
+                                <img src="../images/icons/<?php echo $module_row["module_icon"] ?>" height="100px" width="80px"/>
+                            </h1>
+                            <h4 align="center">
+                                <?php echo $module_row["module_name"];?>
+                            </h4>
+                        </div>
+                    </a>
+                </div>
+            <?php
+                }
+            ?>    
         </div>
     </body>
     <script src="../js/jquery-3.7.1.js"></script>
